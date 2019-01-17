7f25637a3eb15e2762705663335722d279cf2f15
BastiansBits <sebastian.loesch@icloud.com>
2019-01-17 06:12:11 +0100

    Projektdateien hinzufügen.

---
+++ TrayTool/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace TrayTool
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public ObservableCollection<Item> Items { get; set; }
+
+
+        public MainWindow()
+        {
+            Items = new ObservableCollection<Item>()
+            {
+                new Item() { Name = "Item No 1", Path = "Path No 1" },
+                new Item() { Name = "Item No 2", Path = "Path No 2" }
+            };
+
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            string name = tbName.Text;
+            string path = tbPath.Text;
+
+            Item item = new Item()
+            {
+                Name = name,
+                Path = path
+            };
+
+            Items.Add(item);
+        }
+    }
+}
