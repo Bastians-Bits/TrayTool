f8a65dd99f14f5733295482091d02b67b6f58841
BastiansBits <sebastian.loesch@icloud.com>
2019-03-10 20:35:47 +0100

    Simplyfied the panels Screwed up the width's

---
+++ TrayTool/UIControlls/DCItem.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TrayTool.UIControlls
+{
+    /// <summary>
+    /// Interaktionslogik für DCItem.xaml
+    /// </summary>
+    public partial class DCItem : UserControl
+    {
+        public DCItem()
+        {
+            InitializeComponent();
+        }
+    }
+}
